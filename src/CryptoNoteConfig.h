40c16885e3c8b34f39c3b25c154b7c348527e316
Philippe Maquin <philippemaquin2705@gmail.com>
2018-01-25 15:52:45 +0100

    node
    node

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "192.168.1.1:22775",
+  "brogniart-coin.ddns.net:22775",
   //"your_seed_ip2.com:8080",
 };
 
